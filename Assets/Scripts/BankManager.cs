02fb35bdab0c7c8b15ee3e56cb24fab16ebdc8a6
Emirhan Topal <emirhantopal03@hotmail.com>
2024-11-18 22:06:08 +0300

    UI manager added - seperated from BankManager script

--- Assets/Scripts/BankManager.cs
+++ Assets/Scripts/BankManager.cs
@@ -9,7 +9,6 @@ public class BankManager : MonoBehaviour
 {
     [SerializeField] private int startingMoney;
     [SerializeField] int currentMoney; // manipüle edilip fazladan para aktarılabilir o yüzden private
-    [SerializeField] TextMeshProUGUI moneyText;
     public int CurrentMoney // fakat farklı class lardan da erişebilmek için getter ayarladık
     {
         get { return currentMoney; }
@@ -18,26 +17,10 @@ public class BankManager : MonoBehaviour
     {
         currentMoney = startingMoney;
     }
-
-    private void Start()
-    {
-        if (moneyText == null)
-        {
-            moneyText = GetComponent<TextMeshProUGUI>();
-        }
-    }
-
-    private void Update()
-    {
-        Debug.Log(currentMoney);
-        DisplayMoneyUI();
-    }
-
     public void EarnedMoney(int amount) 
     {
         currentMoney += Mathf.Abs(amount); // amount manipüle edip - olursa diye abs kullandık
     }
-
     public void Withdraw(int minusAmount) // minusAmount manipüle edip - olursa diye abs kullandık
     {
         currentMoney -= Mathf.Abs(minusAmount);
@@ -46,12 +29,6 @@ public class BankManager : MonoBehaviour
             GameOver();
         }
     }
-
-    private void DisplayMoneyUI()
-    {
-        moneyText.text = "Gold: " + currentMoney.ToString();
-    }
-    
     private void GameOver()
     {
         int sceneIndex = SceneManager.GetActiveScene().buildIndex;
