0463ff5725faff6165d72ed1b90bca5b24713633
Emirhan Topal <emirhantopal03@hotmail.com>
2024-12-11 23:29:43 +0300

    button added

--- Assets/Scripts/ButtonControl.cs
+++ Assets/Scripts/ButtonControl.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class ButtonControl : MonoBehaviour
 {
     public bool _variatySpaces = false;
+    [SerializeField] private List<ButtonControl> buttonList;
 
     public void PlaceActive()
     {
-            _variatySpaces = !_variatySpaces;
+        foreach (var button in buttonList)
+        {
+            button._variatySpaces = false;
+        }
+        _variatySpaces = !_variatySpaces;
     }
 }
--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 
 // [serializefield] attribute: bir değişkenin private olmasına rağmen Unity'nin Inspector panelinde görünmesini sağlar.
@@ -14,16 +15,19 @@ public class Waypoints : MonoBehaviour
     [SerializeField] private GameObject tower;
     [SerializeField] private GameObject gunTower;
     [SerializeField] private GameObject button;
+    [SerializeField] private GameObject button2;
     [SerializeField] private bool canPlaceGreen;
     [SerializeField] private bool canPlaceBlack;
     private TowerManager _towerManager;
     private ButtonControl _buttonControl;
+    private ButtonControl _buttonControl2;
     private GunTowerManager _gunTowerManager;
     private bool _canBuy;
     private BankManager _bank;
     private void Start()
     {
         _buttonControl = button.GetComponent<ButtonControl>();
+        _buttonControl2 = button2.GetComponent<ButtonControl>();
         _bank = FindObjectOfType<BankManager>();
         _gunTowerManager = FindObjectOfType<GunTowerManager>();
         _towerManager = FindObjectOfType<TowerManager>();
@@ -35,25 +39,44 @@ public class Waypoints : MonoBehaviour
     // {
     //     return isEmpty;
     // }
-    
-    
+
+    private void Update()
+    {
+        canPlaceGreen = _buttonControl._variatySpaces;
+        canPlaceBlack = _buttonControl2._variatySpaces;
+        if (canPlaceGreen)
+        {
+            button.GetComponent<Image>().color = Color.blue;
+            button2.GetComponent<Image>().color = Color.red;
+        }
+        else if (canPlaceBlack)
+        {
+            button2.GetComponent<Image>().color = Color.black;
+            button.GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            button.GetComponent<Image>().color = Color.green;
+            button2.GetComponent<Image>().color = Color.red;
+        }
+    }
+
     //This event is sent to all scripts of the GameObject with Collider. Scripts of the parent or child objects do not receive this event.
     private void OnMouseDown()
     {
         _canBuy = _bank.HaveMoney();
-        canPlaceGreen = _buttonControl._variatySpaces;
-        canPlaceBlack = _buttonControl._variatySpaces;
         if (isEmpty && _canBuy)
         {
-            if (greenPlace & canPlaceGreen)
+            if (greenPlace && canPlaceGreen)
             {
+                
                 Vector3 towerPosition = new Vector3(transform.position.x, transform.position.y + 1.53f, transform.position.z);
                 tower = Instantiate(tower, towerPosition, Quaternion.identity);
                 _bank.Withdraw(_towerManager.CostOfTower);
                 isEmpty = false; // grid space control
                 canPlaceGreen = false;
             }
-            if (blackPlace && canPlaceBlack)
+            else if (blackPlace && canPlaceBlack)
             {
                 Vector3 gunTowerPosition = new Vector3(transform.position.x + 2, transform.position.y + 7.45f, transform.position.z + 2);
                 Instantiate(gunTower, gunTowerPosition, Quaternion.identity);
